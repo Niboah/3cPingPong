0682eaa45e6b99765197942ba7067b4c68b96b2d
Niboah <o.niboah@gmail.com>
2023-03-29 01:21:58 +0200

    remake

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ io.on('connection', (socket) => {
     player1Join=true;
 
     io.to(player1id).emit('join', "player1");
+    if(player1Join && player2Join) io.emit('start', "");
   } else if(!player2Join){
     thisplayer="player2";
     console.log(handshake.address+' is '+thisplayer);
@@ -30,29 +31,28 @@ io.on('connection', (socket) => {
     player2Join=true;
 
     io.to(player2id).emit('join', "player2");
+    if(player1Join && player2Join) io.emit('start', "");
   } 
   else socket.disconnect();
-    
-  socket.on('movePaddle', msg => {
-    if(thisplayer=="player1"){
-      io.emit('player1', msg);
-    }else{
-      io.emit('player2', msg);
-    } 
+
+  socket.on('paddleAPos', msg => {
+    io.to(player2id).emit('paddleAPos',msg);
+  });
+  socket.on('paddleBPos', msg => {
+    io.to(player1id).emit('paddleBPos',msg);
+  });
+  socket.on('resetPoint', msg => {
+    io.emit('resetPoint',"");
   });
 
-  socket.on('ballx', msg => {
-    io.emit('ballx', msg);
-  });
-  socket.on('bally', msg => {
-    io.emit('bally', msg);
-  });
-
-  socket.on('ballout', msg => {
-    io.emit('ballout', msg);
+  socket.on('color', msg => {
+    var color = Math.floor(Math.random() * 3);
+    console.log(color);
+    io.emit('color',color);
   });
 
   socket.on('disconnect', () => {
+    io.emit('pause', "");
     if(thisplayer=="player1") player1Join = false;
     else player2Join=false;
     console.log(thisplayer+' disconnected');
